5dac5f0419c51af118ba9184da4e95fe95fcaf3e
iroliveira <iroliveira@viptech.com.br>
2023-05-10 20:35:25 -0300

    exercicio tde do dia 10/05

--- index.js
+++ index.js
@@ -1,8 +1,78 @@
-const calculadora = require("./calculadora");
+const express = require('express');
+const app = express();
 
-console.log(calculadora.div(5,2));
-console.log(calculadora.mult(5,2));
-console.log(calculadora.sub(5,2));
-console.log(calculadora.sum(5,2));
-console.log(calculadora.par(2));
-console.log(calculadora.par(5));
+app.use(express.json());
+
+app.get('/health', (req, res) => {
+  res.json({
+    status: 'Running',
+  });
+});
+
+let tasks = [
+  {
+    id: 1,
+    name: 'Comprar leite',
+    description: 'Ir no mercado da esquina e comprar leite',
+    isDone: false,
+  },
+  {
+    id: 2,
+    name: 'Comprar pão',
+    description: 'Ir no mercado da esquina e comprar pão',
+    isDone: false,
+  },
+];
+
+app.get('/tasks', (req, res) => {
+  res.json({
+    tasks: tasks,
+  });
+});
+
+app.post('/tasks', (req, res) => {
+  const newTask = {
+    id: tasks.length + 1,
+    name: req.body.name,
+    description: req.body.description,
+    isDone: req.body.isDone,
+  };
+  tasks.push(newTask);
+
+  res.json({
+    tasks: {
+      res: 'Tarefa criada com sucesso',
+      task: newTask,
+    },
+  });
+});
+
+app.put('/tasks/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const task = tasks.find((it) => it.id === id);
+
+  if (!task) return res.status(404).json({ message: 'Tarefa não encontrada' });
+
+  task.name = req.body.name;
+  task.description = req.body.description;
+  task.isDone = req.body.isDone;
+
+  res.json({
+    res: 'Tarefa atualizada com sucesso',
+    task: task,
+  });
+});
+
+app.delete('/tasks/:id', (req, res) => {
+  const id = Number(req.params.id);
+  tasks = tasks.filter((it) => it.id != id);
+
+  res.json({
+    res: 'Tarefa deletada com sucesso',
+  });
+});
+
+const port = 8080;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
